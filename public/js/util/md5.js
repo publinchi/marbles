e66430ea10beed7f14ee27f41635fa3a36c6a781
Publio Estupiñán <publinchi@gmail.com>
2019-09-01 13:58:57 -0500

    Se modifica algoritmo

--- public/js/util/md5.js
+++ public/js/util/md5.js
@@ -1,34 +1,13 @@
-function calcMD5(f) {
-    // MD5 Calculation
-    var blobSlice = File.prototype.slice || File.prototype.mozSlice || File.prototype.webkitSlice,
-    file = f,
-    chunkSize = 1024 * 1024,                             // Read in chunks of 2MB
-    chunks = Math.ceil(file.size / chunkSize),
-    currentChunk = 0,
-    spark = new SparkMD5.ArrayBuffer(),
-    fileReader = new FileReader();
-
-    fileReader.onload = function (e) {
-        console.log('read chunk nr', currentChunk + 1, 'of', chunks);
-        spark.append(e.target.result);                   // Append array buffer
-        currentChunk++;
-
-        if (currentChunk < chunks) {
-            loadNext();
-        } else {
-            console.log('finished loading');
-            console.info('computed hash', spark.end());  // Compute hash
-        }
-    };
-
-    fileReader.onerror = function () {
-        console.warn('oops, something went wrong.');
-    };
-    function loadNext() {
-        var start = currentChunk * chunkSize,
-            end = ((start + chunkSize) >= file.size) ? file.size : start + chunkSize;
-
-        fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
+function fileHash( file, hasher, callback ){
+    //Instantiate a reader		  
+    var reader = new FileReader();
+          
+    //What to do when we gets data?
+    reader.onload = function( e ){
+        var hash = hasher(e.target.result);
+        callback( hash );
     }
-    loadNext();
+        
+    reader.readAsBinaryString( file );
+
 }
